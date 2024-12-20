55c982534b9f75d7286b3e636b1b9b7b861bedea
Troy Benson <troybensonsa@gmail.com>
2024-12-20 16:36:15 +0000

    testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
-pub mod schema;
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_mul() {
+        assert_eq!(mul(2, 3), 6);
+    }
+}
